c0dc63e732aa8b8f89a6e3c8186251fc16183ee5
Emily Ryan <emilyryan@Emilys-MacBook-Pro.local>
2020-12-03 17:37:47 -0600

    ccompleted writing tests

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import ContactForm from './ContactForm';
+
+test('renders without errors', () => {
+    render(<ContactForm/>);
+})
+
+test('display Edd Burke as placeholder on page load', () => {
+    render(<ContactForm/>)
+
+    const edd = screen.findByPlaceholderText(/edd/i);
+
+    waitFor(() => {
+        expect(edd.toBeInTheDocument());
+    })
+})
+
+test('can input information and submit in ContactForm', () => {
+    render(<ContactForm/>);
+
+    const firstNameInput = screen.getByLabelText(/First Name*/i);
+    const lastNameInput = screen.getByLablelText(/Last Name*/i);
+    const emailInput = screen.getByLabelText(/Email*/i);
+    const messageInput = screen.getByLabelText(/Message/i);
+
+    fireEvent.change(firstNameInput, { target: { value: 'Jonathan'} });
+    fireEvent.change(lastNameInput, { target: { value: 'David'} });
+    fireEvent.change(emailInput, { target: { value: 'jd@gmail.com'} });
+    fireEvent.change(messageInput, { target: { value: 'Hey there'} });
+
+    const submitBtn = screen.getByRole('button', {name: /submit/i });
+    
+    fireEvent.click(submitBtn);
+
+})
